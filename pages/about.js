a759ef0961d22b27a6dedfffa1d0bb4e3801611e
burczu <b.dybowski@gmail.com>
2018-01-25 15:51:06 +0100

    another route added

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default () => (
+  <div>
+    <div>
+      <Link href="/"><a>Home</a></Link>
+    </div>
+    <hr />
+    <div>About the page here!</div>
+  </div>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,15 @@
-export default () => <div>Hello world!</div>;
+import Link from 'next/link';
+
+const Index = (props) => {
+  return (
+    <div>
+      <div>
+        <Link href="/about"><a>About</a></Link>
+      </div>
+      <hr />
+      <div>Hello world!</div>
+    </div>
+  );
+}
+
+export default Index;
